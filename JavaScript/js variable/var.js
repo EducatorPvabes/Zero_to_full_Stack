6bdb189664845beface413b6bafc294481cfba69
Pranshi Verma <pranshi.verma@abes.ac.in>
2026-03-27 23:58:39 +0530

    JS Variable added

---
+++ JavaScript/js variable/var.js
@@ -0,0 +1,40 @@
+//variable
+//var
+//let
+//const
+
+// //var
+// var a = 10;
+// console.log(a); //10
+// var a = 20;
+// console.log(a); //20
+
+// if (true) {
+//     var b = 30;
+// }
+// console.log(b); //30
+
+//let
+// const
+
+// let 
+// let marks = 90;
+// console.log(marks);
+// let marks = 95; //SyntaxError: Identifier 'marks' has already been declared
+// console.log(marks);
+
+// if
+//     (true) {
+//     let marks1 = 85;
+//     }
+// console.log(marks1); //85
+
+let name = "John";
+console.log(name);
+
+// const
+const pi = 3.14;
+console.log(pi);
+ pi = 3.15159; 
+ console.log(pi); //SyntaxError: Identifier 'pi' has already been declared
+ //TypeError: Assignment to constant variable.
